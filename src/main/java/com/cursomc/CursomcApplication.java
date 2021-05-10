9420dd0aef5b0b9ca3654e288156985f9ae24fe5
Danilo William dos Reis Socorro <d.dos.reis.socorro@avanade.com>
2021-05-10 17:06:16 -0300

    Estado e Cidades

--- src/main/java/com/cursomc/CursomcApplication.java
+++ src/main/java/com/cursomc/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.cursomc.domain.Categoria;
+import com.cursomc.domain.Cidade;
+import com.cursomc.domain.Estado;
 import com.cursomc.domain.Produto;
 import com.cursomc.repositories.CategoriaRepository;
+import com.cursomc.repositories.CidadeRepository;
+import com.cursomc.repositories.EstadoRepository;
 import com.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,12 @@ public class CursomcApplication implements CommandLineRunner{
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -35,6 +45,25 @@ public class CursomcApplication implements CommandLineRunner{
 		Produto p2 = new Produto(null,"Impressora", 800.00);
 		Produto p3 = new Produto(null,"Mouse", 30.00);
 		
+		Estado minas = new Estado(null, "Minas Gerais");
+		Estado saoPaulo = new Estado(null, "São Paulo");
+		
+		Cidade capitalSp  = new Cidade(null, "São Paulo Capital");
+		Cidade campinas = new Cidade(null, "Campinas");
+		
+		Cidade uberlandia = new Cidade(null, "Uberlândia");
+		Cidade beloHorizonte = new Cidade(null, "Belo Horizonte");
+		
+		uberlandia.setEstado(minas);
+		beloHorizonte.setEstado(minas);
+		
+		campinas.setEstado(saoPaulo);
+		capitalSp.setEstado(saoPaulo);
+		
+		minas.getCidades().addAll(Arrays.asList(uberlandia, beloHorizonte));
+		saoPaulo.getCidades().addAll(Arrays.asList(capitalSp, campinas));
+		
+		
 		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
 		
@@ -45,7 +74,9 @@ public class CursomcApplication implements CommandLineRunner{
 		
 		repository.saveAll(Arrays.asList(cat1, cat2));
         produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
-		
+		estadoRepository.saveAll(Arrays.asList(saoPaulo, minas));
+		cidadeRepository.saveAll(Arrays.asList(capitalSp, beloHorizonte, uberlandia, campinas));
+
 	}
 	
 	
