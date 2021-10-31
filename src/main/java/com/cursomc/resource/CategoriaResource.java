73e3dd384926ad07edaf4146896adbb2ce474e8e
Danilo Wiliam <danilowilliam87@outlook.com>
2021-10-31 11:50:50 -0300

    criado endpont para salvar uma categoria

--- src/main/java/com/cursomc/resource/CategoriaResource.java
+++ src/main/java/com/cursomc/resource/CategoriaResource.java
@@ -2,12 +2,12 @@ package com.cursomc.resource;
 
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.cursomc.domain.Categoria;
 import com.cursomc.service.CategoriaService;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/categoria")
@@ -23,5 +23,15 @@ public class CategoriaResource {
 	public ResponseEntity<?> find(@PathVariable Integer id) throws Throwable {
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok(obj);
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri()
+				.path("/{id}")
+				.buildAndExpand(obj.getId())
+				.toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
